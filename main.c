4329ee73d7e051ab2178de1cdad43f239e219d50
Juanpi <juanpablocomella@gmail.com>
2025-04-27 07:16:13 +0200

    Función para leer el CSV

--- main.c
+++ main.c
@@ -3,6 +3,30 @@
 #include <stdlib.h>
 #include "interfaces.h"
 
+#define MAX_LINE_LENGTH 512
+
+void leerCSV(const char *filename) {
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL) {
+        perror("Error al abrir el archivo");
+        return;
+    }
+
+    char line[MAX_LINE_LENGTH];
+    while (fgets(line, sizeof(line), file)) {
+    line[strcspn(line, "\n")] = 0; // Remove newline character
+
+    char *letra = strtok(line, ";");      // Get first part before ;
+    char *palabra = strtok(NULL, ";");    // Get second part before ;
+    char *definicion = strtok(NULL, ";"); // Get last part
+
+    if (letra && palabra && definicion) {
+        // Agregar a la estructura de datos donde se guardan las palabras
+    }
+}
+}
+
 int main(void) {
     primeraInterfaz();
 
