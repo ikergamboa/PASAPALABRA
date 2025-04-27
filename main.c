1c8b32fa72e0f7d2dbaed52bcef9fa7b70b1110c
Juanpi <juanpablocomella@gmail.com>
2025-04-27 15:49:31 +0200

    Creado el diccionario con las palabras utilizadas para el juego

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <time.h>
 #include "interfaces.h"
 
 #define MAX_LINE_LENGTH 512
@@ -18,7 +19,7 @@ typedef struct {
 } Almacen; // Esta estructura almacena las palabras por cada letra.
 
 typedef struct {
-   Almacen almacen[26]; // 26 letras del alfabeto.
+   Almacen almacen[25]; // 25 letras del alfabeto.
    int totalPreguntas;
 } DiccionarioJuego; // Aquí guardamos las palabras que se utilizarán en el juego.
 
@@ -66,12 +67,64 @@ void leerCSV(const char *filename) {
     char *definicion = strtok(NULL, ";");
 
     if (letra && palabra && definicion) {
-        // Agregar a la estructura de datos donde se guardan las palabras
+      crearAlmacenPorLetra(letra[0], palabra, definicion);
+        }
     }
 }
+
+DiccionarioJuego crearDiccionarioJuego() {
+    DiccionarioJuego diccionario = {0};
+    srand(time(NULL));
+
+    for (int i = 0; i < 26; i++) {
+        if (almacen[i].size > 0) {
+            int randIndex = rand() % almacen[i].size; // se selecciona un valor random entre 0 y el tamaño de la lista de palabras -1 (10)
+
+            Entrada entradaSeleccionada = almacen[i].entradas[randIndex]; // se crea una entrada con la palabra y definición seleccionada
+            diccionario.almacen[i].entradas[0] = entradaSeleccionada;
+            diccionario.almacen[i].size = 1;
+            diccionario.totalPreguntas++;
+        }
+    }
+
+    // ACCESO A LAS PALABRAS Y DEFINICIONES DEL DICCIONARIO CREADO
+
+    // Para la palabra
+    //diccionario.almacen[i].entradas[0].palabra
+    // Para la definición
+    //diccionario.almacen[i].entradas[0].definicion
+
+    return diccionario;
 }
 
 int main(void) {
+
+    leerCSV("palabras.csv");
+    DiccionarioJuego juego = crearDiccionarioJuego();
+
+    for (int i = 0; i < 26; i++) {
+        if (almacen[i].size > 0) {
+            printf("\nLetra %c (%d palabras):\n", 'A' + i, almacen[i].size);
+            for (int j = 0; j < almacen[i].size; j++) {
+                printf("  Palabra: %s\n", almacen[i].entradas[j].palabra);
+                printf("  Definicion: %s\n", almacen[i].entradas[j].definicion);
+                printf("---\n");
+            }
+        }
+    }
+
+    for (int i = 0; i < 26; i++) {
+        if (juego.almacen[i].size > 0) {
+            printf("\nLetra %c (%d palabras):\n", 'A' + i, juego.almacen[i].size);
+            for (int j = 0; j < juego.almacen[i].size; j++) {
+                printf("  Palabra: %s\n", juego.almacen[i].entradas[j].palabra);
+                printf("  Definicion: %s\n", juego.almacen[i].entradas[j].definicion);
+                printf("---\n");
+            }
+        }
+    }
+
+
     primeraInterfaz();
 
     return 0;
